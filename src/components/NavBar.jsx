c4dfea260e811d6bc8500f92806aa4c8521cb332
Santiago Marquinez <abogmarquinez@gmail.com>
2023-10-25 17:32:35 -0300

    .

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import CartWidget from './CartWidget'
+import { Menu, MenuButton, MenuList, MenuItem, Flex, Box, Spacer } from '@chakra-ui/react'
 
 const NavBar = () => {
     return (
         <div>
-            <h3>Brand</h3>
-            <CartWidget/>
+
+            <Flex>
+                <Box>
+                    <img src="../assets/images/logocuervo.png" alt="logocuervo" />
+                    <h3>Brand</h3>
+                </Box>
+                <Spacer/>
+                <Spacer/>
+                <Box>
+                    <Menu>
+                        <MenuButton>
+                            Categorias
+                        </MenuButton>
+                        <MenuList>
+                            <MenuItem>Camisetas</MenuItem>
+                            <MenuItem>Camperas</MenuItem>
+                            <MenuItem>Shorts</MenuItem>
+                            <MenuItem>Medias</MenuItem>
+                        </MenuList>
+                    </Menu>
+                </Box>
+
+                <Spacer/>
+                <Box>
+                    <CartWidget />
+                </Box>
+            </Flex>
+
+            
         </div>
     )
 }
