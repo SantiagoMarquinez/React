9d7d6ac6c7391f7d710d163fb9790cfb92457834
Santiago Marquinez <abogmarquinez@gmail.com>
2023-10-25 11:36:28 -0300

    primer commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CartWidget = () => {
+    return (
+        <div>Carrito
+            5</div>
+    )
+}
+
+export default CartWidget
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+
+const NavBar = () => {
+    return (
+        <div>
+            <h3>Brand</h3>
+            <CartWidget/>
+        </div>
+    )
+}
+
+export default NavBar
