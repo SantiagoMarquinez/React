e3d5b67da9d8a34a737704c4ec0a7ee2451632aa
Santiago Marquinez <abogmarquinez@gmail.com>
2023-11-02 20:45:24 -0300

    Hice la logica para que el contador no pase de 10 ni baje de 0, agregue toast, modifique estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
+import ItemCount from './components/ItemCount'
 
 const App = () => {
   return (
     <div>
       <ItemListContainer greeting={"Bienvenido a Cuervo Store"} />
       <NavBar />
-
+      <ItemCount/>
     </div>
   )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import { Flex, Text } from '@chakra-ui/react'
+import React from 'react';
+import { Flex, Text } from '@chakra-ui/react';
 const ItemListContainer = ({ greeting }) => {
+    const productos = [
+        { id: 1, nombre: "Producto 1", precio: 20 },
+        { id: 2, nombre: "Producto 2", precio: 35 },
+        { id: 3, nombre: "Producto 3", precio: 48 },
+        { id: 4, nombre: "Producto 4", precio: 60 },
+    ]
+    const validacion = true;
+    const promesa = new Promise((resolve, reject) => {
+        if (validacion) {
+            setTimeout(() => {
+                resolve("Productos validados");
+                console.log(productos);
+            }, 3000);
+        } else {
+            reject("Error en la consulta");
+        }
+    });
+    promesa
+    .then((resultado) =>{
+        console.log(resultado)
+    })
+    .catch((error) =>{
+        console.log(error)
+    });
     return (
         <div>
-            <Flex align='center'justify='center'>
+            <Flex align='center' justify='center'>
                 <Text fontSize='4xl'>
                     {greeting}
                 </Text>
